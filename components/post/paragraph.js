bd0dba5486fc66a24cd98396eeaee33ef3f9ea50
Vlad Samoylov <samoylov.dev@gmail.com>
2018-03-13 04:21:15 +0300

    experiment failed

--- components/post/paragraph.js
+++ components/post/paragraph.js
@@ -1,18 +1,14 @@
-export default class extends React.Component {
-    render() {
-        return (
-            <p ref={ref => this.ref = ref}>
-                {this.props.children}
-                {/*language=CSS*/}
-                <style jsx>{`
-                    p {
-                        line-height: 41px;
-                        font-size: 28px;
-                        margin: 0 0 24px;
-                        width: 680px;
-                    }
-                `}</style>
-            </p>
-        )
-    }
-}
+export default (props) => (
+    <p>
+        {props.children}
+        {/*language=CSS*/}
+        <style jsx>{`
+            p {
+                line-height: 41px;
+                font-size: 28px;
+                margin: 0 0 24px;
+                width: 680px;
+            }
+        `}</style>
+    </p>
+)
